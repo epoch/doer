de5c0d696352d17c3b1e8c785f2ff3bf6780f98e
Daniel Tsui <tsui.daniel@gmail.com>
2016-10-10 12:02:48 +1100

    add jquery

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+console.log('this is the app.js');
+
 var addBtn = document.getElementById('add-btn');
 var newTodoInput = document.getElementById('new-todo-input');
 var todoList = document.getElementById('todo-list');
@@ -7,8 +9,21 @@ var addTodo = function() {
   var newLI = document.createElement('li');
   var newContent = document.createTextNode(newTodoContent);
   newLI.appendChild(newContent);
+
+  newLI.addEventListener('click', function(event) {
+    // console.log(event.target);
+    event.target.className = 'done';
+  });
+
   todoList.appendChild(newLI);
+  newTodoInput.value = ''; // clearing the input
 }
 
 
 addBtn.addEventListener('click', addTodo);
+
+var listItems = document.getElementsByTagName('li');
+
+// for (var i = 0; i < listItems.length; i++) {
+
+// }
